2320f697da8bb674e44539bf378a0c6e9f793e51
werifu <wohu1363195380@outlook.com>
2019-10-25 21:35:18 +0800

    用的是递归

--- factorial.sh
+++ factorial.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 # 用来计算阶乘
-sum=1
-n=$1
-i=1
-while(( $i<=$n ))
-do
-	let "sum*=i"
-	let "i++"
-done
-echo $sum 
-
+	
+f() 
+{
+	local n=$1
+	if [ $n -eq 1 ]
+	then
+		rtn=1
+	else
+		f `expr $n - 1`
+		rtn=`expr $n \* $rtn`
+	fi
+	
+	return $rtn
+	
+}
+f $1
+echo $rtn
